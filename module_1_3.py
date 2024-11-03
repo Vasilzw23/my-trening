974aa19d6d422e45c80987da2601b9eb57ed7cd5
VasilVera928 <154725408+Vasilzw23@users.noreply.github.com>
2024-11-04 00:54:59 +0900

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,16 @@
+from tkinter.font import names
+
+name = "Name:Vera"
+print(name)
+age = "Age:41"
+print(age)
+new = "New age:42"
+print(new)
+is_student = "is_student:True"
+print(is_student)
+
+a = 'number of completed HW:12'
+b = 'number of hours spent:1.5'
+c = 'course name:"Python"'
+d = 'time for one task:0.12'
+print(c, a, b, d)
